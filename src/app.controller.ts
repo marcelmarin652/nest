21651ec42073fe229373c380ba549138c32ba644
Marcel Marin <marcelmarin256@gmail.com>
2022-12-22 08:06:59 -0400

    Uso de get recibiendo parametros query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -11,31 +11,41 @@ export class AppController {
   }
 
   @Get('nuevo')
-  newEndPoint(){
-    return'Yo soy nuevo';
+  newEndPoint() {
+    return 'Yo soy nuevo';
   }
 
   @Get('/ruta/')
-  hello(){
-    return'Yo soy  con /sas/';
+  hello() {
+    return 'Yo soy  con /sas/';
   }
 
-  //:id es un parametro
-  @Get('/product/:productId')
-  getProduct(@Param() params:any){
-    // tiene que ser params.id porque tiene que ser el mismo nombre que el parametro como lo pongamos
-    return`Product ${params.productId}`;
+  @Get('/products/')
+  getProduct(
+    @Query('limit') limit: number = 100,
+    @Query('offset') offset: number = 0,
+    @Query('brand') brand: string,
+  ) {
+    // puedo enviar algo asi http://localhost:3000/products?limit=100&offset=50
+    return `Product: limit=>${limit} offset=> ${offset} brand=> ${brand}`;
+  }
+
+  // aqui colocamos filter arriba para que no se confunda con el de abajo ya que puede pensar que "filter" es un id
+  @Get('/products/filter')
+  getProductFilter(){
+   // tiene que ser params.id porque tiene que ser el mismo nombre que el parametro como lo pongamos
+  return`Soy un filtro`;
   }
 
   @Get('/products/:productId')
   getProducts(@Param('productId') productId:string){
-    // tiene que ser params.id porque tiene que ser el mismo nombre que el parametro como lo pongamos
-    return`Product ${productId}`;
+   // tiene que ser params.id porque tiene que ser el mismo nombre que el parametro como lo pongamos
+  return`Product ${productId}`;
   }
 
   @Get('/categories/:id/products/:productId')
-  getCategory(@Param('productId') productId:string, @Param('id') id:string){
+  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
     // tiene que ser params.id porque tiene que ser el mismo nombre que el parametro como lo pongamos
-    return`Product ${productId} and ${id}`;
+    return `Product ${productId} and ${id}`;
   }
 }
