5625af76e35515123f162f51594d78d9a88ca17b
Marcel Marin <marcelmarin256@gmail.com>
2022-12-21 20:23:27 -0400

    Uso de get y recibir parametros

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,35 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hola Mundo';
+  }
+
+  @Get('nuevo')
+  newEndPoint(){
+    return'Yo soy nuevo';
+  }
+
+  @Get('/ruta/')
+  hello(){
+    return'Yo soy  con /sas/';
+  }
+
+  //:id es un parametro
+  @Get('/product/:productId')
+  getProduct(@Param() params:any){
+    // tiene que ser params.id porque tiene que ser el mismo nombre que el parametro como lo pongamos
+    return`Product ${params.productId}`;
+  }
+
+  @Get('/products/:productId')
+  getProducts(@Param('productId') productId:string){
+    // tiene que ser params.id porque tiene que ser el mismo nombre que el parametro como lo pongamos
+    return`Product ${productId}`;
+  }
+
+  @Get('/categories/:id/products/:productId')
+  getCategory(@Param('productId') productId:string, @Param('id') id:string){
+    // tiene que ser params.id porque tiene que ser el mismo nombre que el parametro como lo pongamos
+    return`Product ${productId} and ${id}`;
   }
 }
