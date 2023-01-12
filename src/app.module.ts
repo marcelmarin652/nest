e837d66915a78f635f15d28e4a8df0fa8bbdcd9b
Marcel Marin <marcelmarin256@gmail.com>
2023-01-12 11:10:08 -0400

    Se separo la logica de los productos en un modulo para poder ir organizando el proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './controllers/products.controller';
-import { CategoriesController } from './controllers/categories.controller';
-import { BrandsController } from './controllers/brands.controller';
 import { UsersController } from './controllers/users.controller';
 import { CostumersController } from './controllers/costumers.controller';
 import { OrdersController } from './controllers/orders.controller';
-import { ProductsService } from './services/products/products.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController, CategoriesController, BrandsController, UsersController, CostumersController, OrdersController],
-  providers: [AppService, ProductsService],
+  imports: [UsersModule, ProductsModule],
+  controllers: [AppController, UsersController, CostumersController, OrdersController],
+  providers: [AppService,],
 })
 export class AppModule {}
---
+++ src/products/products.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+
+import { BrandsController } from './controllers/brands.controller';
+import { CategoriesController } from './controllers/categories.controller';
+import { ProductsController } from './controllers/products.controller';
+
+import { BrandsService } from './services/brands/brands.service';
+import { CategoriesService } from './services/categories/categories.service';
+import { ProductsService } from './services/products/products.service';
+
+@Module({
+  controllers: [BrandsController, CategoriesController, ProductsController],
+  providers: [BrandsService, CategoriesService, ProductsService],
+  
+})
+export class ProductsModule {}
