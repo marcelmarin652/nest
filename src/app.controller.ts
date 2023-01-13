be876389f32929709660a21aca8f2ee906f257ef
Marcel Marin <marcelmarin256@gmail.com>
2023-01-13 14:47:56 -0400

    Pruebas con useValue y como variable de entorno

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return 'Hola Mundo';
+    return this.appService.getHello();
   }
 
   @Get('nuevo')
@@ -22,5 +22,5 @@ export class AppController {
 
 
 
-  
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,25 @@ import { OrdersController } from './controllers/orders.controller';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY2 = '123456';
+const API_KEY = 'prod123142124';
+
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController, CostumersController, OrdersController],
-  providers: [AppService,],
+  providers: [
+    AppService,
+    {
+      //PROVIDE SERIA COMO EL NOMBRE DE LA VARIABLE
+      provide: 'API_KEY2',
+      // el usevalue seria el valor que le doy a la variable, la cual esta declarada arriba
+      useValue: API_KEY,
+    },
+    {
+      //Aqui estamos haciedno que el valor de la variable sea dinamico segun en el modo en que se este ejecutando la app (dev o prod)
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY : API_KEY2,
+    }
+  ],
 })
 export class AppModule {}
