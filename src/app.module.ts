51f61e320d78445e52ed5bfa091d26cbbc3c827b
Marcel Marin <marcelmarin256@gmail.com>
2023-01-13 21:10:56 -0400

    Uso de useFactor para hacer una solicitud asincrona usando axios, me dio varios problemas pero los dejo en el notion, igualmente tuve que intalar, rxjs para poder leer el observable y instalar axios, corri varios audit fix force y npm install force para que funcionara bien todo, y tuve que agregar unos headers a la peticion para que no me diera error

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,21 @@ import { OrdersController } from './controllers/orders.controller';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+
 const API_KEY2 = '123456';
 const API_KEY = 'prod123142124';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [AppController, CostumersController, OrdersController],
   providers: [
     AppService,
@@ -24,7 +34,20 @@ const API_KEY = 'prod123142124';
       //Aqui estamos haciedno que el valor de la variable sea dinamico segun en el modo en que se este ejecutando la app (dev o prod)
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY : API_KEY2,
-    }
+    },
+    {
+      provide: 'TASKS',
+      inject: [HttpService],
+      useFactory: async (http: HttpService) => {
+        const tasks = await http.get(
+          'https://jsonplaceholder.typicode.com/todos',{
+            headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+          }
+        );
+        const value = Promise.resolve(firstValueFrom(tasks));
+        return value;
+      },
+    },
   ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,9 +3,13 @@ import { Injectable, Inject } from '@nestjs/common';
 @Injectable()
 export class AppService {
 
-  constructor(@Inject('API_KEY') private apiKey: string) {}
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    @Inject('TASKS') private tasks: any[], // 👈 inject TASKS
+  ) {}
 
   getHello(): string {
+    console.log(this.tasks); // 👈 print TASKS
     return `Hello World! ${this.apiKey}`;
   }
 }
